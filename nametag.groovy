a2cbdc8569367304ea623dd084052df6abe90f3f
raylopez <raylopez@localhost>
2023-09-18 14:42:15 -0400

    Centered the text to fit on the name tag base
    teacher instructed to do so

--- nametag.groovy
+++ nametag.groovy
@@ -1,13 +1,32 @@
-// code here
+
+CSG text = CSG.text("Ray",5)
+
+CSG rotatedText = text.rotz(90)
+
+double distanceyCenter = rotatedText.getCenterY()
+
+println "Center of Word "+distanceyCenter
+
+CSG centeredText = rotatedText.movey(-distanceyCenter)
 
 double width = 30
 double depth = 70
 double height = 3
+double widthOfText = centeredText.getTotalY()+5.0
 
+double scale = depth/widthOfText
+
+CSG scaledText = centeredText.scaley(scale)
+
+double distanceInX = scaledText.getCenterX()
+CSG textCenterInX = scaledText.movex(-distanceInX)
+
+println " scale for text "+scale
+println "width of text "+widthOfText
 println "width is "+width
 println "height is "+height
 println "depth is "+depth
 
 CSG nameTagBase = new Cube(width,depth,height) . toCSG()
 
-return nameTagBase
+return [nameTagBase,textCenterInX]
